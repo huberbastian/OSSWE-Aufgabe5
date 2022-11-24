7014685d267f25811b29e030c2e9520a1ec12e9e
Bastian Huber <bastihu02@gmail.com>
2022-11-24 17:39:55 +0100

    app

--- aufgabe5/src/main/java/de/hfu/App.java
+++ aufgabe5/src/main/java/de/hfu/App.java
@@ -3,7 +3,7 @@ package de.hfu;
 import java.util.Scanner;
 
 public class App {
-
+    
     public static void main( String[] args ) {
         Scanner s = new Scanner(System.in);
         System.out.print("Text: "); 
